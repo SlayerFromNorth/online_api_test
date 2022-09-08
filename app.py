9adee850d30524f597651885f1d1b6fb8a23ff20
SlayerFromNorth <mariusoa@gmail.com>
2022-09-08 13:55:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
 from flask import Flask, request, jsonify
-from fastai.basic_train import load_learner
-from fastai.vision import open_image
-from flask_cors import CORS,cross_origin
+from PIL import Image
+
+
+import fastbook
+fastbook.setup_book()
+
+from fastbook import *
+
+
+path = Path()
+learn = load_learner(path/'export.pkl')
+
+
+
 app = Flask(__name__)
-CORS(app, support_credentials=True)
 
-# load the learner
-learn = load_learner(path='./models', file='export.pkl')
-classes = learn.data.classes
+@app.route("/marius", methods=["POST"])
+def process_image():
+    file = request.files['image']
+    # Read the image via file.stream
+    img = Image.open(file.stream)
+    img.save("testerIMG.jpg")
+    prediction = learn.predict("testerIMG.jpg")
+    return jsonify({'msg': 'success', 'size': prediction[0]})
 
-
-def predict_single(img_file):
-    'function to take image and return prediction'
-    prediction = learn.predict(open_image(img_file))
-    probs_list = prediction[2].numpy()
-    return {
-        'category': classes[prediction[1].item()],
-        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
-    }
-
-
-# route for prediction
-@app.route('/predict', methods=['POST'])
-def predict():
-    return jsonify(predict_single(request.files['image']))
-
-if __name__ == '__main__':
-    app.run()
+import os
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+        port=int(os.getenv('PORT', 3342)))
